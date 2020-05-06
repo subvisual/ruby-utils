0e259122ba99d91a7f52a1540945b20cb93c5e66
Pedro Costa <dev@iampfac.com>
2020-05-06 20:04:58 +0100

    :sunrise:

---
+++ lib/ruby/utils.rb
@@ -0,0 +1,8 @@
+require "ruby/utils/version"
+
+module Ruby
+  module Utils
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/ruby/utils_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Ruby::Utils do
+  it "has a version number" do
+    expect(Ruby::Utils::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
